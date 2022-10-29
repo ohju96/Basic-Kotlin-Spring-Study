1f9831fb13c2c7605ac518802e0f11b87978ffb8
ohju96 <ohju96@naver.com>
2022-10-29 21:33:37 +0900

    이슈 상세 조회

--- issue-service/first-service/src/main/kotlin/com/test/issueservice/service/IssueService.kt
+++ issue-service/first-service/src/main/kotlin/com/test/issueservice/service/IssueService.kt
@@ -3,8 +3,10 @@ package com.test.issueservice.service
 import com.test.issueservice.domain.Issue
 import com.test.issueservice.domain.IssueRepository
 import com.test.issueservice.domain.enums.IssueStatus
+import com.test.issueservice.exception.NotFoundException
 import com.test.issueservice.model.IssueRequest
 import com.test.issueservice.model.IssueResponse
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -32,4 +34,10 @@ class IssueService(
     fun getAll(status: IssueStatus) =
         issueRepository.findAllByStatusOrderByCreatedAtDesc(status)
             ?.map { IssueResponse(it) }
+
+    @Transactional(readOnly = true)
+    fun get(id: Long): IssueResponse {
+        val issue = issueRepository.findByIdOrNull(id) ?: throw NotFoundException("이슈 존재하지 않습니다.")
+        return IssueResponse(issue)
+    }
 }
