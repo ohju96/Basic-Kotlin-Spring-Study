57f69f475cce8109e530b28aeebd0cfddb073c8b
ohju96 <ohju96@naver.com>
2022-11-05 18:17:09 +0900

    애노테이션 컨트롤러

---
+++ webFluxTest/src/main/kotlin/com/example/webfluxtest/book/BookService.kt
@@ -0,0 +1,47 @@
+package com.example.webfluxtest.book
+
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import java.util.concurrent.atomic.AtomicInteger
+
+data class Book(val id: Int, val name: String, val price: Int)
+
+@Service
+class BookService {
+
+    private final val nextId = AtomicInteger(0)
+
+    val books = mutableListOf(
+        Book(id = nextId.incrementAndGet(), name = "코틀린 인 액션", price = 30000),
+        Book(id = nextId.incrementAndGet(), name = "Http 완벽 가이드", price = 40000),
+    )
+
+    fun getAll(): Flux<Book> {
+        return Flux.fromIterable(books)
+    }
+
+    fun get(id: Int): Mono<Book> {
+        return Mono.justOrEmpty(books.find { it.id == id })
+    }
+
+    fun add(request: Map<String, Any>): Mono<Book> {
+        return Mono.just(request)
+            .map { map ->
+                val book = Book(
+                    id = nextId.incrementAndGet(),
+                    name = map["name"].toString(),
+                    price = map["price"] as Int
+                )
+                books.add(book)
+                book
+            }
+    }
+
+    fun delete(id: Int): Mono<Void> {
+        return Mono.justOrEmpty(books.find { it.id == id })
+            .map { books.remove(it) }
+            .then()
+    }
+
+}
