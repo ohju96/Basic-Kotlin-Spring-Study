4489c5b529546da15b774c556051f8fcf5835a96
ohju96 <ohju96@naver.com>
2022-11-12 17:31:54 +0900

    JWT 토큰 기반 인증

---
+++ issue-service/user-service/src/main/kotlin/com/ohju/userservice/config/JWTProperties.kt
@@ -0,0 +1,14 @@
+package com.ohju.userservice.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "jwt")
+class JWTProperties(
+    val issuer: String,
+    val subject: String,
+    val expiresTime: Long,
+    val secret: String,
+
+)
